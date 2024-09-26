ef0af77b3c875fdab3556d1c9580c47bd620c927
coco-droid <friseurdefeise@gmail.com>
2024-09-26 08:52:41 -0400

    Refactor btn-signup component to update donation link

--- src/components/common/btn-signup.jsx
+++ src/components/common/btn-signup.jsx
@@ -4,7 +4,7 @@ const SignupButton = () => {
   return (
     <>
   <div className='w-[200px] flex justify-around'>
-   <a href='/donate'>
+   <a href='/donation'>
     <button className="items-center text-white   bg-[#981a3c] text-white justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground  h-10 px-4 py-2 hidden md:inline-flex">
            Donate
      </button>
