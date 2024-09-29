0f6b21dbaa6c94760ccf1eaaa3a10657b038a0fe
coco-droid <friseurdefeise@gmail.com>
2024-09-29 01:11:35 -0400

    Refactor LandingPage component to update carousel and header layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,17 +11,18 @@ const LandingPage = () => {
     return (
         <>
             <Header />
-            <div className="flex flex-col md:flex-row h-full mb-[250px] relative">
-                <Carousel />
-                <div className="z-10 absolute flex flex-col items-center justify-center px-4 md:px-12 md:h-[200px] rounded-[12px] bg-transparent gap-1 w-full md:w-[200px] md:absolute md:top-[220px]">
-                    <span className="p-2 rounded-[12px] text-[#000] bg-white bg-opacity-5 backdrop-blur-sm text-xl md:text-[2.5em] font-bold mb-3 text-center">
-                        Bienvenue sur le site de l’Association pour l’Évangélisation des Enfants
-                    </span>
-                    <p className="p-3 rounded-[12px] text-[#000] text-lg md:text-[1.8em] flex items-center font-bold bg-white bg-opacity-10 backdrop-blur-sm w-full md:w-4/5 text-center">
-                        De la Région Nord/Sud Amérique et Caraïbes Créolophone et Francophone
-                    </p>
-                </div>
-            </div>
+            <div className="flex flex-col md:flex-row h-full  relative rounded-[12px]">
+  <Carousel />
+  <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center p-4 rounded-[12px] bg-white bg-opacity-30 w-full">
+    <span className="text-[#000] text-xl md:text-[2.5em] font-bold mb-3 text-center">
+      Bienvenue sur le site de l’Association pour l’Évangélisation des Enfants
+    </span>
+    <p className="text-[#000] text-lg md:text-[1.8em] font-bold text-center">
+      De la Région Nord/Sud Amérique et Caraïbes Créolophone et Francophone
+    </p>
+  </div>
+</div>
+
             <About />
             <Ministries />
             <WeeklyWord />
