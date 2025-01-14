ce37fe134849eabf986845b868ab38edb5d9e03c
coco-droid <friseurdefeise@gmail.com>
2025-01-14 11:36:46 -0500

    Refactor LandingPage component to improve text formatting and readability in the welcome message

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -9,28 +9,33 @@ import Blog from '../components/layout/Blog';
 import Popsurvey from "../components/common/popsurvey";
 const LandingPage = () => {
     return (
-        <>
-            <Header />
-            <div className="flex flex-col md:flex-row h-full  relative rounded-[12px]">
-  <Carousel />
-  <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center p-4 rounded-[12px] bg-white bg-opacity-30 w-full">
-    <span className="text-[#000] text-xl md:text-[2.5em] font-bold mb-3 text-center">
-      Bienvenue sur le site de l’Association pour l’Évangélisation des Enfants
-    </span>
-    <p className="text-[#000] text-lg md:text-[1.8em] font-bold text-center">
-      De la Région Nord/Sud Amérique et Caraïbes Créolophone et Francophone
-    </p>
-  </div>
-</div>
-<div className="flex flex-col items-center w-screen  justify-center">
-            <About />
-            <Ministries />
-            <WeeklyWord />
-            <Blog />
-            <Popsurvey />
-            </div>
-            <Footer />
-        </>
+      <>
+        <Header />
+        <div className="flex flex-col md:flex-row h-full relative rounded-[12px]">
+          <Carousel />
+          <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center p-4 rounded-[12px] bg-white bg-opacity-30 w-full">
+            <span className="text-[#000]  text-xl md:text-[2.5em] font-bold mb-3 text-center">
+              Bienvenue sur le site de <br/>
+              l'Association pour <br/>
+              l'Évangélisation des 
+              Enfants
+            </span>
+            <p className="text-[#000] text-lg md:text-[1.8em] font-bold text-center">
+              De la Région Nord/Sud 
+              Amérique et Caraïbes 
+              Créolophone et Francophone
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center w-screen justify-center">
+          <About />
+          <Ministries />
+          <WeeklyWord />
+          <Blog />
+          <Popsurvey />
+        </div>
+        <Footer />
+      </>
     );
 };
 
