63850bc47b8c76169010c46d71124051ed3a7c66
Robe <friseurdefeise@gmail.com>
2026-01-22 16:35:04 -0500

    feat: Add dynamic export to CMS API route and improve Supabase client initialization

--- app/api/cms/route.ts
+++ app/api/cms/route.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/services/supabase.conf";
 import { NextResponse, NextRequest } from "next/server";
+
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
